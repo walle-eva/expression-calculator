cf6dec4154d51f2a732aa7c79f44f82a6b55adca
walle-eva <walle-eva@users.noreply.github.com>
2020-03-19 00:03:30 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let result = 0;
+	let arrExpr = expr.split('');
+	
+	for(let i = 0; i < arrExpr.length; i++){
+	  if(/[\+\*-\/]/.test(arrExpr[i])){
+	    if(arrExpr[i] == '*'){
+		  result = +arrExpr[i - 1] * +arrExpr[i + 1];
+		}
+		if(arrExpr[i] == '/'){
+		  result = +arrExpr[i - 1] / +arrExpr[i + 1];
+		}
+		if(arrExpr[i] == '+'){
+		  result = +arrExpr[i - 1] + +arrExpr[i + 1];
+		}
+		if(arrExpr[i] == '-'){
+		  result = +arrExpr[i - 1] - +arrExpr[i + 1];
+		}
+	  }
+	}
+	return result;
 }
 
 
