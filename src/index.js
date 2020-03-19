89f532e8e9a5e829bad959aefbfa990bc44809d5
walle-eva <walle-eva@users.noreply.github.com>
2020-03-19 21:58:27 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,26 +4,35 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    let result = 0;
-	let arrExpr = expr.split('');
+    if(expr.includes(' ')){
+	  arr = expr.split(' ');
+	} else {
+	  arr = expr.split('');
+	}
 	
-	for(let i = 0; i < arrExpr.length; i++){
-	  if(/[\+\*-\/]/.test(arrExpr[i])){
-	    if(arrExpr[i] == '*'){
-		  result = +arrExpr[i - 1] * +arrExpr[i + 1];
-		}
-		if(arrExpr[i] == '/'){
-		  result = +arrExpr[i - 1] / +arrExpr[i + 1];
-		}
-		if(arrExpr[i] == '+'){
-		  result = +arrExpr[i - 1] + +arrExpr[i + 1];
-		}
-		if(arrExpr[i] == '-'){
-		  result = +arrExpr[i - 1] - +arrExpr[i + 1];
-		}
+	for(let i = 0; i < arr.length; i++){
+	  if(arr[i] == ' ' || arr[i] == ''){
+	    arr.splice(i, 1);
+	  }
+	} 
+	let result = arr[0];
+	for(let i = 1; i < arr.length; i = i+2){
+	  console.log(result)
+	  if(arr[i] == '+'){
+	    result += arr[i + 1];
+	  }
+	  if(arr[i] == '-'){
+	    result -= arr[i + 1];
+	  }
+	  if(arr[i] == '*'){
+	    result *= arr[i + 1];
+	  }
+	  if(arr[i] == '/'){
+	    result /= arr[i + 1];
+		console.log(result)
 	  }
 	}
-	return result;
+	return +result.toFixed(4);
 }
 
 
